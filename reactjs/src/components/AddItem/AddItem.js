a929c5b72d9522f235b761377edf09fdc18e03be
MiloszBryla <bryla.milosz@gmail.com>
2021-09-15 22:08:26 +0200

    Refactor add-item page

--- reactjs/src/components/AddItem/AddItem.js
+++ reactjs/src/components/AddItem/AddItem.js
@@ -7,6 +7,7 @@ import Cookies from "js-cookie";
 import jwt_decode from "jwt-decode";
 import {useHistory} from "react-router-dom";
 import { Redirect } from 'react-router-dom'
+import TemplateProfileImage from "../Dashboard/DashboardComponents/templateProfileImage.svg";
 
 function AddItem() {
     const history = useHistory();
@@ -72,57 +73,102 @@ function AddItem() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <h1 id="lets-get-started">Let's get started! </h1>
-            <h2 id="show-what-u-got"> Show us what you've got</h2>
+        // <form onSubmit={handleSubmit(onSubmit)}>
+        //     <h1 id="lets-get-started">Let's get started! </h1>
+        //     <h2 id="show-what-u-got"> Show us what you've got</h2>
+        //
+        //     <div className="inputs">
+        //         <h4>What's your item?</h4>
+        //         <input className="add-item-item-name" placeholder="" name="name" ref={register({required: true, minLength: 3})}/>
+        //         {errors.name &&
+        //         <p className="error-message">OwnedItem name is too short!</p>}
+        //     <br/>
+        //
+        //
+        //         <div id="input-image">
+        //             <a><img id="icon" src={require('../../assets/icon-with-mountain.svg')}/></a>
+        //             <a href="#" className="upload-image"> Click to upload image</a>
+        //         </div>
+        //         <h2 className="add-item-h2"> Describe your item</h2>
+        //         <h4><label htmlFor="quest-type">Categories</label></h4>
+        //         <select className="type-selector" id="quest-type" name="category.id" ref={register}>
+        //             <option value="" selected disabled hidden>Choose...</option>
+        //             {category.map(element => (
+        //                 <option value={element.id}>{element.description}</option>
+        //             ))} </select>
+        //
+        //         {/*<p><br/>trzeba wybrać usera  nie mamy sesji:</p>*/}
+        //         {/*<select className="type-selector" id="quest-type" name="owner.id" ref={register}>*/}
+        //         {/*    <option value="" selected disabled hidden>Choose...</option>*/}
+        //         {/*    {owners.map(element => (*/}
+        //         {/*        <option value={element.id}>{element.firstName + element.lastName}</option>*/}
+        //         {/*    ))} </select>*/}
+        //
+        //
+        //         <h4>Description:</h4>
+        //         <textarea className="item-description" name="description" placeholder="Describe your item..."
+        //                   ref={register({required: true, minLength: 10})}/>
+        //         {errors.description &&
+        //         <p className="error-message">Description is too short!</p>}
+        //
+        //         <h2 className="add-item-h2">How much? </h2>
+        //         <h4>Price (PLN/per day):</h4>
+        //         <input className="item-price" name="price" placeholder="Your price per day" ref={register({required: true, minLength: 1})}/>
+        //
+        //         <h6>
+        //             By Clicking "Add item for rent", you agree to our <a href="#">Terms</a> and that you have
+        //             read our <a href="#"> Data Policy</a>.
+        //         </h6>
+        //         <div className="add-item-submit-button">
+        //             <button className="button" type="submit">Add item for rent</button>
+        //         </div>
+        //     </div>
+        // </form>
+        <div style={{width: "70%", height: "60%", margin: "auto", marginTop: "100px"}}>
+            <div className="dashboard-content-container acc-account" style={{color: "black", borderRadius: "8px"}}>
+                <h7>Add Item</h7>
+                <form className="account-settings-form" style={{borderRadius: "12px"}} onSubmit={handleSubmit(onSubmit)}>
 
-            <div className="inputs">
-                <h4>What's your item?</h4>
-                <input className="add-item-item-name" placeholder="" name="name" ref={register({required: true, minLength: 3})}/>
-                {errors.name &&
-                <p className="error-message">OwnedItem name is too short!</p>}
-            <br/>
+                    <input name="id" value={""} ref={register()} style={{display: "none"}}/>
+
+                    <div className="account-settings-section acc-name">
+                        <p className="acc-title"></p>
+                        <div className="acc-input-container acc-fist-name">
+                            <p className="acc-input-label">Name</p>
+                            <input className="acc-input" name="name" defaultValue={""}
+                                   ref={register()}/>
+                            {"" && <p className="error-message">Name is too short!</p>}
+                        </div>
+                        <div className="acc-input-container acc-last-name">
+                            <p className="acc-input-label">Description</p>
+                            <textarea className="acc-input" defaultValue={""} name="description"
+                                   ref={register()}/>
+                            {"" && <p className="error-message">Last name is too short!</p>}
+                        </div>
+                        <div className="acc-input-container acc-email">
+                            <p className="acc-input-label">Category</p>
+                            <select className="acc-input" defaultValue={""} name="category.id"
+                                   ref={register()}><option value="" selected disabled hidden>Choose...</option>
+                                             {category.map(element => (
+                                                 <option value={element.id}>{element.description}</option>))} </select>
+                            {"" && <p className="error-message">E-mail is too short!</p>}
+                        </div>
+                        <div className="acc-input-container acc-email">
+                            <p className="acc-input-label">Price per day</p>
+                            <input style={{width: "150px", margin: "auto"}} type="number" min="0" step="0.50" className="acc-input" name="price"></input>
+                        </div>
+                    </div>
+                    <div className="account-settings-section acc-img">
+                        <p className="acc-input-label">Upload images</p>
+                        <input className="acc-input" type="file" min="0" step="0.50" className="acc-input" name="files" multiple></input>
+                                            </div>
 
 
-                <div id="input-image">
-                    <a><img id="icon" src={require('../../assets/icon-with-mountain.svg')}/></a>
-                    <a href="#" className="upload-image"> Click to upload image</a>
-                </div>
-                <h2 className="add-item-h2"> Describe your item</h2>
-                <h4><label htmlFor="quest-type">Categories</label></h4>
-                <select className="type-selector" id="quest-type" name="category.id" ref={register}>
-                    <option value="" selected disabled hidden>Choose...</option>
-                    {category.map(element => (
-                        <option value={element.id}>{element.description}</option>
-                    ))} </select>
 
-                {/*<p><br/>trzeba wybrać usera  nie mamy sesji:</p>*/}
-                {/*<select className="type-selector" id="quest-type" name="owner.id" ref={register}>*/}
-                {/*    <option value="" selected disabled hidden>Choose...</option>*/}
-                {/*    {owners.map(element => (*/}
-                {/*        <option value={element.id}>{element.firstName + element.lastName}</option>*/}
-                {/*    ))} </select>*/}
-
-
-                <h4>Description:</h4>
-                <textarea className="item-description" name="description" placeholder="Describe your item..."
-                          ref={register({required: true, minLength: 10})}/>
-                {errors.description &&
-                <p className="error-message">Description is too short!</p>}
-
-                <h2 className="add-item-h2">How much? </h2>
-                <h4>Price (PLN/per day):</h4>
-                <input className="item-price" name="price" placeholder="Your price per day" ref={register({required: true, minLength: 1})}/>
-
-                <h6>
-                    By Clicking "Add item for rent", you agree to our <a href="#">Terms</a> and that you have
-                    read our <a href="#"> Data Policy</a>.
-                </h6>
-                <div className="add-item-submit-button">
-                    <button className="button" type="submit">Add item for rent</button>
-                </div>
+                    <button className="acc-button acc-save-changes">Add item</button>
+                </form>
             </div>
-        </form>
+        </div>
     )
 
 }
